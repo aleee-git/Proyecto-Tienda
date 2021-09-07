a59f536b911fd0e8751714c74060a0e537d8651b
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2021-09-07 16:01:47 -0600

    Mostrar productos y footer

---
+++ Proyecto_Grupal_Inventario/Controllers/ProductoController.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Grupal_Inventario.Controllers
+{
+    public class ProductoController : Controller
+    {
+        //Objeto contextp
+        private Contexto contexto = new Contexto();
+
+        public ActionResult MostrarProductos ()
+        {
+            var model = contexto.Productos.ToList();
+            return View(model);
+        }
+
+
+    }
+}
